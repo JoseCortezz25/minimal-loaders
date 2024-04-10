9b422658dd508b9c2daab82dcb50ba99d01ff497
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-04-09 20:35:53 -0500

    Add loaders to index page

---
+++ src/components/common/Loaders/Loaders.tsx
@@ -0,0 +1,23 @@
+const LoaderL1 = () => {
+  return (
+    <div className='loader-l1'></div>
+  )
+}
+
+const LoaderL2 = () => {
+  return (
+    <div className='loader-l2'></div>
+  )
+}
+
+const LoaderL3 = () => {
+  return (
+    <div className='loader-l3'></div>
+  )
+}
+
+export {
+  LoaderL1,
+  LoaderL2,
+  LoaderL3
+}
