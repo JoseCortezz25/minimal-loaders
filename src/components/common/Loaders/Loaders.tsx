dca41609b6c774e9f2efe18015f585ff9dd6522a
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-04-14 19:19:35 -0500

    Update styling in Banner, Header, Loaders, and Footer components

--- src/components/common/Loaders/Loaders.tsx
+++ src/components/common/Loaders/Loaders.tsx
@@ -1,23 +1,51 @@
 const LoaderL1 = () => {
   return (
-    <div className='loader-l1'></div>
+    <div className="loader-l1"></div>
   )
 }
 
 const LoaderL2 = () => {
   return (
-    <div className='loader-l2'></div>
+    <div className="loader-l2"></div>
   )
 }
 
 const LoaderL3 = () => {
   return (
-    <div className='loader-l3'></div>
+    <div className="loader-l3"></div>
+  )
+}
+
+const LoaderL4 = () => {
+  return (
+    <div className="loader-l4"><div></div><div></div></div>
+  )
+}
+
+const LoaderL5 = () => {
+  return (
+    <div className="loader-l5"></div>
+  )
+}
+
+const LoaderL6 = () => {
+  return (
+    <div className="loader-l6"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+const LoaderL7 = () => {
+  return (
+    <div className="loader-l7"><div></div><div></div><div></div><div></div></div>
   )
 }
 
 export {
   LoaderL1,
   LoaderL2,
-  LoaderL3
+  LoaderL3,
+  LoaderL4,
+  LoaderL5,
+  LoaderL6,
+  LoaderL7
 }
